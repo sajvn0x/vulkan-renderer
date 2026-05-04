f1ba6b8eab2c83daf22ad8da82f99fe41b30d8b9
sajeevan <sajeevanjspy@gmail.com>
2026-05-04 12:56:13 +0530

    chore: add glfw and vma libraries - initialize glfw window

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,24 @@
+#include <GLFW/glfw3.h>
+
 #include "renderer/driver_context.hh"
 
 int main(void) {
-    RenderingDriverContext driver_context = RenderingDriverContext();
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
+    RenderingDriverContext driver_context = RenderingDriverContext();
     if (driver_context.initialize() != OK) {
         return -1;
     }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
