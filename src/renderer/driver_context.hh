439b6e1a272fc35112931b0cf49697bfc765b568
sajeevan <sajeevanjspy@gmail.com>
2026-05-07 17:51:59 +0530

    chore: move context types to 'types.hh'

--- src/renderer/driver_context.hh
+++ src/renderer/driver_context.hh
@@ -6,33 +6,7 @@
 
 #include "core/containers.hh"
 #include "core/error/error_list.hh"
-
-struct Vendor {
-    constexpr static uint32_t VENDOR_UNKNOWN = 0x0;
-    constexpr static uint32_t VENDOR_AMD = 0x1002;
-    constexpr static uint32_t VENDOR_IMGTEC = 0x1010;
-    constexpr static uint32_t VENDOR_APPLE = 0x106B;
-    constexpr static uint32_t VENDOR_NVIDIA = 0x10DE;
-    constexpr static uint32_t VENDOR_ARM = 0x13B5;
-    constexpr static uint32_t VENDOR_MICROSOFT = 0x1414;
-    constexpr static uint32_t VENDOR_QUALCOMM = 0x5143;
-    constexpr static uint32_t VENDOR_INTEL = 0x8086;
-};
-
-enum DeviceType {
-    DEVICE_TYPE_OTHER = 0x0,
-    DEVICE_TYPE_INTEGRATED_GPU = 0x1,
-    DEVICE_TYPE_DISCRETE_GPU = 0x2,
-    DEVICE_TYPE_VIRTUAL_GPU = 0x3,
-    DEVICE_TYPE_CPU = 0x4,
-    DEVICE_TYPE_MAX = 0x5
-};
-
-struct Device {
-    String name = "Unknown";
-    uint32_t vendor = Vendor::VENDOR_UNKNOWN;
-    DeviceType type = DEVICE_TYPE_OTHER;
-};
+#include "types.hh"
 
 class RenderingDriverContext {
    private:
--- src/renderer/types.hh
+++ src/renderer/types.hh
@@ -6,11 +6,54 @@
 
 #include "core/containers.hh"
 
+/* driver context */
+struct VulkanApiVersion {
+    uint32_t api_version = VK_API_VERSION_1_0;
+
+    bool supports(uint32_t version) const { return api_version >= version; }
+
+    bool supports_vulkan_1_1() const { return supports(VK_API_VERSION_1_1); }
+
+    bool supports_vulkan_1_2() const { return supports(VK_API_VERSION_1_2); }
+
+    bool supports_vulkan_1_3() const { return supports(VK_API_VERSION_1_3); }
+
+    bool supports_vulkan_1_4() const { return supports(VK_API_VERSION_1_4); }
+};
+
+struct Vendor {
+    constexpr static uint32_t VENDOR_UNKNOWN = 0x0;
+    constexpr static uint32_t VENDOR_AMD = 0x1002;
+    constexpr static uint32_t VENDOR_IMGTEC = 0x1010;
+    constexpr static uint32_t VENDOR_APPLE = 0x106B;
+    constexpr static uint32_t VENDOR_NVIDIA = 0x10DE;
+    constexpr static uint32_t VENDOR_ARM = 0x13B5;
+    constexpr static uint32_t VENDOR_MICROSOFT = 0x1414;
+    constexpr static uint32_t VENDOR_QUALCOMM = 0x5143;
+    constexpr static uint32_t VENDOR_INTEL = 0x8086;
+};
+
+enum DeviceType {
+    DEVICE_TYPE_OTHER = 0x0,
+    DEVICE_TYPE_INTEGRATED_GPU = 0x1,
+    DEVICE_TYPE_DISCRETE_GPU = 0x2,
+    DEVICE_TYPE_VIRTUAL_GPU = 0x3,
+    DEVICE_TYPE_CPU = 0x4,
+    DEVICE_TYPE_MAX = 0x5
+};
+
+struct Device {
+    String name = "Unknown";
+    uint32_t vendor = Vendor::VENDOR_UNKNOWN;
+    DeviceType type = DEVICE_TYPE_OTHER;
+};
+
+/* driver device */
 struct Size2i {
     float x, y;
 };
 
-/* capabilities */
+// capabilities
 struct SubgroupCapabilities {
     uint32_t size = 0;
     uint32_t min_size = 0;
